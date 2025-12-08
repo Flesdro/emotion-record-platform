ad8836e9cebd60f92b744648a8d772c0b22f7b9a
Eyvind Wang <eyvindwang1123@gmail.com>
2025-12-08 10:17:26 +0800

    first commit

---
+++ src/main/java/com/jue/emotion_record_platform/EmotionRecordPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.jue.emotion_record_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmotionRecordPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmotionRecordPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jue/emotion_record_platform/EmotionRecordPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jue.emotion_record_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmotionRecordPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
